c082d40a958abcc41cdc57a59f2834ab82f28e27
Alfons Caroles <cllin7787@gmail.com>
2017-04-23 17:40:29 +1000

    fix js and comment

--- JustDoIt_App/app/assets/javascripts/map.js
+++ JustDoIt_App/app/assets/javascripts/map.js
@@ -8,16 +8,16 @@ $(document).ready(function(){
         lng: position.coords.longitude
       };
 
-  //     $.ajax({
-  //       url: '/message',
-  //       method: 'get',
-  //       data: { lat: pos.lat,
-  //               lng: pos.lng
-  //             }
-  //       }).done(function(data){
-  //         console.log(data);
-  //       });
-  //   });
-  // };
+      // $.ajax({
+      //   url: '/message',
+      //   method: 'get',
+      //   data: { lat: pos.lat,
+      //           lng: pos.lng
+      //         }
+      //   }).done(function(data){
+      //     console.log(data);
+      //   });
+    });
+  };
 
 });
