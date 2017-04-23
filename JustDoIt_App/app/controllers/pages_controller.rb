07aa6c8e8a63c72cfe980bbc0c2ef24fc1f94d08
Alfons Caroles <cllin7787@gmail.com>
2017-04-23 14:12:32 +1000

    Remove map features on pages controller

--- JustDoIt_App/app/controllers/pages_controller.rb
+++ JustDoIt_App/app/controllers/pages_controller.rb
@@ -10,22 +10,5 @@ class PagesController < ApplicationController
     if current_user.passenger != nil
       @current_car = Car.find(current_user.passenger.car_id)
     end
-
-    @current_location = []
-
-    if params[:lat] != @current_location[0] && params[:lat].present?
-      @current_location << params[:lat]
-      @current_location << params[:lng]
-    end
-
-    @distances = []
-
-    @cars.each do |i|
-      meeting_point = []
-      meeting_point << i.latitude
-      meeting_point << i.longitude
-      Geocoder.configure(:units => :km)
-      @distances << Geocoder::Calculations.distance_between(@current_location, meeting_point).round(2)
-    end
   end
 end
