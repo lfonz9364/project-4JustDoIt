85dab5c0485ae53f87e072954f4c60d4d958e99c
Barry Tabios <b_tabios@yahoo.com>
2017-04-23 10:33:06 +1000

    updated

--- JustDoIt_App/app/controllers/pages_controller.rb
+++ JustDoIt_App/app/controllers/pages_controller.rb
@@ -5,20 +5,21 @@ class PagesController < ApplicationController
   def message
     @cars = Car.all
     @current_location = []
-    @current_location << params[:current_latitude]
-    @current_location << params[:current_longitude]
+
+    if params[:lat] != @current_location[0] && params[:lat].present?
+      @current_location << params[:lat]
+      @current_location << params[:lng]
+      byebug
+    end
 
     @distances = []
 
     @cars.each do |i|
-
       meeting_point = []
       meeting_point << i.latitude
       meeting_point << i.longitude
-
       Geocoder.configure(:units => :km)
       @distances << Geocoder::Calculations.distance_between(@current_location, meeting_point).round(2)
-      byebug
     end
 
   end
