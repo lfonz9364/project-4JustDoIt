d7273efce7550e1a9fc8e4c6371ca84529cb166f
Barry Tabios <b_tabios@yahoo.com>
2017-05-18 19:19:29 +1000

    geolocation complete

--- JustDoIt_App/app/controllers/api/locations_controller.rb
+++ JustDoIt_App/app/controllers/api/locations_controller.rb
@@ -1,9 +1,9 @@
 class Api::LocationsController < ApplicationController
 
   def index
-
-    @cars = Car.find_by(id: params[:id])
-    # @cars = Car.all
+    #
+    # @cars = Car.find_by(id: params[:id])
+    @cars = Car.all
     render json: @cars
   end
 
