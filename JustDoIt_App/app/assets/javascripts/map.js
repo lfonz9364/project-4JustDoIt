b3e94dbcd22f8072bf6918a29d666e414b844081
Barry Tabios <b_tabios@yahoo.com>
2017-05-18 22:21:03 +1000

    updated;

---
+++ JustDoIt_App/app/assets/javascripts/map.js
@@ -0,0 +1,27 @@
+console.log('added');
+
+$(document).ready(function(){
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      $.ajax({
+        url: '/message',
+        method: 'get',
+        data: { lat: pos.lat,
+                lng: pos.lng
+              }
+        }).done(function(data){
+          console.log(data);
+        });
+    });
+  };
+
+});
+
+$(function(){
+  $("#geocomplete").geocomplete()
+});
