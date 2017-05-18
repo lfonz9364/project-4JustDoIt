678b4f82ea92e12fd612b2610c7b69a449e8acc6
Marta Scaramella <marta.scaramella@gmail.com>
2017-05-18 14:55:39 +1000

    geocomplete working

--- JustDoIt_App/app/assets/javascripts/application.js
+++ JustDoIt_App/app/assets/javascripts/application.js
@@ -14,4 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require geocomplete
-//= require 'map'
+//= require map
--- JustDoIt_App/app/assets/javascripts/map.js
+++ JustDoIt_App/app/assets/javascripts/map.js
@@ -21,3 +21,7 @@ $(document).ready(function(){
   };
 
 });
+
+$(function(){
+  $("#geocomplete").geocomplete()
+});
